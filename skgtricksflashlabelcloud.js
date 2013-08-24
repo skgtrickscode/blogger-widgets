142d5ab555922c78faf16614f428e9b078169867
skgtrickscode <gargshivam111999@gmail.com>
2013-08-24 19:02:51 +0530

    Update skgtricksflashlabelcloud.js

--- skgtricksflashlabelcloud.js
+++ skgtricksflashlabelcloud.js
@@ -1,11 +1,11 @@
 /**
  * Auto Flash Label Cloud Script for Blogger
- * Author  : Shivam garg from skgtricks.blogspot.com
+ * Author  : Harish
  * Version : 1.1
  * (c)2012 All Rights Reserved by Author
  *
  * 	==== Default Options ====
- * 	var SkgtricksFlashLabelSettings = {
+ * 	var NBTFlashLabelSettings = {
  *		blogurl        : "http//" + location.host,
  *		color          : "000000",
  *		hoverColor     : "333333",
@@ -18,58 +18,4 @@
  *	};
  *
  */
-if (typeof SkgtricksFlashLabelSettings == 'undefined') {
-    var SkgtricksFlashLabelSettings = {}
-}
-var SkgtricksFlashLabelSettings = {
-    blogurl: SkgtricksFlashLabelSettings.blogurl || 'http://' + location.host,
-    color: SkgtricksFlashLabelSettings.color || '000000',
-    hoverColor: SkgtricksFlashLabelSettings.hoverColor || '333333',
-    backgroundColor: SkgtricksFlashLabelSettings.backgroundColor || 'FFFFFF',
-    size: SkgtricksFlashLabelSettings.size || 15,
-    speed: SkgtricksFlashLabelSettings.speed || 100,
-    width: SkgtricksFlashLabelSettings.width || 250,
-    height: SkgtricksFlashLabelSettings.height || 300,
-    transparency: (typeof SkgtricksFlashLabelSettings.transparency == 'undefined') ? true : SkgtricksFlashLabelSettings.transparency
-};
-
-function generatelabels(json) {
-    var SkgtricksFlashTags = '',
-        Skgtrickstags = new Array('<tags>'),
-        SkgtricksCat = json.feed.category;
-    for (i = 0; i < SkgtricksCat.length; i++) {
-        Skgtrickstags.push('<a href="' + SkgtricksFlashLabelSettings.blogurl + '/search/label/' + SkgtricksCat[i].term + '/" style="' + SkgtricksFlashLabelSettings.size + '">' + SkgtricksCat[i].term + '</a>')
-    }
-    Skgtrickstags.push('</tags>');
-    SkgtricksFlashTags = encodeURIComponent(Skgtrickstags.join(''));
-    var flashvars = {};
-    flashvars.tcolor = '0x' + SkgtricksFlashLabelSettings.color;
-    flashvars.hicolor = '0x' + SkgtricksFlashLabelSettings.hoverColor;
-    flashvars.mode = 'tags';
-    flashvars.distr = 'true';
-    flashvars.tspeed = SkgtricksFlashLabelSettings.speed;
-    flashvars.tagcloud = SkgtricksFlashTags;
-    var params = {};
-    if (SkgtricksFlashLabelSettings.transparency) {
-        params.wmode = 'transparent'
-    }
-    params.bgcolor = '#' + SkgtricksFlashLabelSettings.backgroundColor;
-    params.allowscriptaccess = 'always';
-    var attributes = {};
-    attributes.id = 'SkgtricksFlashLabels';
-    swfobject.embedSWF('https://dl.dropbox.com/s/9jz6tzalqkxqchi/Skgtricks-tagcloud.swf', 'SkgtricksFlashContent', SkgtricksFlashLabelSettings.width, SkgtricksFlashLabelSettings.height, '9.0.0', 'http://swfobject.googlecode.com/svn-history/r322/trunk/swfobject/expressInstall.swf', flashvars, params, attributes)
-}(function () {
-    var SkgtricksBody = document.getElementsByTagName('head')[0];
-    var SkgtricksswfObject = document.createElement('script');
-    SkgtricksswfObject.type = 'text/javascript';
-    SkgtricksswfObject.src = 'https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js';
-    SkgtricksBody.appendChild(SkgtricksswfObject);
-    var SkgtricksFlashElem = document.getElementById('SkgtricksFlashContent');
-    if (SkgtricksFlashElem === null) {
-        document.write('<div id=\'SkgtricksFlashContent\'></div>')
-    }
-    var SkgtricksJsonScript = document.createElement('script');
-    SkgtricksJsonScript.type = 'text/javascript';
-    SkgtricksJsonScript.src = SkgtricksFlashLabelSettings.blogurl + '/feeds/posts/summary?alt=json&callback=generatelabels&max-results=1';
-    SkgtricksBody.appendChild(SkgtricksJsonScript)
-})();
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('k(K 3==\'I\'){4 3={}}4 3={b:3.b||\'E://\'+1b.1c,j:3.j||\'1a\',n:3.n||\'19\',u:3.u||\'16\',m:3.m||15,r:3.r||17,v:3.v||18,o:3.o||1d,d:(K 3.d==\'I\')?C:3.d};A M(w){4 q=\'\',f=1e 1j(\'<t>\'),g=w.14.1k;1i(i=0;i<g.1h;i++){f.J(\'<a 1f="\'+3.b+\'/1g/1l/\'+g[i].H+\'/" Z="\'+3.m+\'">\'+g[i].H+\'</a>\')}f.J(\'</t>\');q=13(f.12(\'\'));4 5={};5.X=\'Q\'+3.j;5.11=\'Q\'+3.n;5.W=\'t\';5.V=\'C\';5.1s=3.r;5.F=q;4 6={};k(3.d){6.1J=\'1I\'}6.1m=\'#\'+3.u;6.1G=\'1H\';4 l={};l.T=\'1N\';7.1Q(\'S://1O.1P.h/s/1M/1D-F.z\',\'p\',3.v,3.o,\'9.0.0\',\'E://7.1r.h/1q-1n/1o/1p/7/1u.z\',5,6,l)}(A(){4 x=8.1v(\'1B\')[0];4 c=8.P(\'O\');c.U=\'D/B\';c.y=\'S://R.1C.h/R/1A/7/2.2/7.1z\';x.G(c);4 L=8.1w(\'p\');k(L===1x){8.1y(\'<N T=\\\'p\\\'></N>\')}4 e=8.P(\'O\');e.U=\'D/B\';e.y=3.b+\'/1t/1E/1F?1L=w&1K=M&Y-10=1\';x.G(e)})();',62,115,'|||NBTFlashLabelSettings|var|flashvars|params|swfobject|document|||blogurl|NBTswfObject|transparency|NBTJsonScript|NBTtags|NBTCat|com||color|if|attributes|size|hoverColor|height|NBTFlashContent|NBTFlashTags|speed||tags|backgroundColor|width|json|NBTBody|src|swf|function|javascript|true|text|http|tagcloud|appendChild|term|undefined|push|typeof|NBTFlashElem|generatelabels|div|script|createElement|0x|ajax|https|id|type|distr|mode|tcolor|max|style|results|hicolor|join|encodeURIComponent|feed||FFFFFF|100|250|333333|000000|location|host|300|new|href|search|length|for|Array|category|label|bgcolor|history|r322|trunk|svn|googlecode|tspeed|feeds|expressInstall|getElementsByTagName|getElementById|null|write|js|libs|head|googleapis|NBT|posts|summary|allowscriptaccess|always|transparent|wmode|callback|alt|9jz6tzalqkxqchi|NBTFlashLabels|dl|dropbox|embedSWF'.split('|'),0,{}))
