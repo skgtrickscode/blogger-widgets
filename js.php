9434e4e1fca92fbcff9ee018a2ab93e0df9c1df2
skgtrickscode <gargshivam111999@gmail.com>
2013-08-25 09:33:11 +0530

    Update js.php

--- js.php
+++ js.php
@@ -1,5 +1,5 @@
 	(function(d){
-	  var l = d.getElementById("foxyform_embed_link_464344");
+	 
 	      i = d.createElement('IFRAME');
 	  i.src = "http://www.foxyform.com/form.php?id=464344&sec_hash=e584865e973";
 	  i.width = "350px";
@@ -7,7 +7,6 @@
 	  i.frameborder = 0;
 	  i.setAttribute('style', 'border: none;');
 	  i.setAttribute('frameborder', '0');
-	  l.setAttribute('style', 'display: block; width: 350px; font-size: 0.7em; color: grey; text-align: right; text-decoration: none;');
-	  l.parentNode.insertBefore(i,l);
+
 	})(document);
 		
